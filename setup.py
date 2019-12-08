aff633b5546333f4bb8c5a7564f8a70204dbd3ea
Konstantin Novichikhin <konstantin.novichikhin@gmail.com>
2019-12-08 16:55:35 -0500

    Add license and description content type

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 from iso8583 import __version__
 
 classifiers = [
@@ -10,14 +10,13 @@ classifiers = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: Implementation :: CPython",
     "Programming Language :: Python :: Implementation :: PyPy",
 ]
 
 if __name__ == "__main__":
 
-    with open('README.rst') as f:
+    with open("README.rst", "r", encoding="utf-8") as f:
         readme = f.read()
 
     setup(
@@ -27,9 +26,11 @@ if __name__ == "__main__":
         author_email="konstantin.novichikhin@gmail.com",
         description="A serializer and deserializer of ISO8583 data.",
         long_description=readme,
+        long_description_content_type="text/x-rst",
+        license="MIT",
         url="https://github.com/manoutoftime/pyiso8583",
-        packages=['iso8583'],
+        packages=["iso8583"],
         classifiers=classifiers,
-        python_requires='>=3.6',
-        keywords='iso8583 8583',
+        python_requires=">=3.6",
+        keywords="iso8583 8583",
     )
