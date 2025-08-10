ee6f2f76f05ccccb938f5b9efc73cbacb0bf1369
Konstantin Novichikhin <konstantin.novichikhin@gmail.com>
2025-08-10 02:45:49 -0400

    4.0.0 release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     setup(
         name="pyiso8583",
-        version="3.0.0",
+        version="4.0.0",
         author="Konstantin Novichikhin",
         author_email="konstantin.novichikhin@gmail.com",
         description="A serializer and deserializer of ISO8583 data.",
